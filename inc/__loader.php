f6a0ffdcdf4cd98bd7a469aa1d3a4fd7a18de033
Jesse Corkill <xxjescorkillxx@gmail.com>
2021-09-30 16:00:10 -0500

    Clean Slate

--- inc/__loader.php
+++ inc/__loader.php
@@ -4,11 +4,8 @@
     public static function init(){
 
       //Load all inc files
-      self::load('acf');
-      //self::load('acf-blocks');
-      self::load('enqueue');
-      self::load('features');
-      self::load('cpt-floorplan');
+      //self::load('filename');
+
 
     }
     private static function load($file){
